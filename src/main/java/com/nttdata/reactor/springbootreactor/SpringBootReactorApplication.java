fe1537d88534ec952a7a08c8523d56e7e76e7a63
JostinSamuel <jostinmartinez1605@gmail.com>
2022-08-29 18:37:41 -0500

    first commit

---
+++ src/main/java/com/nttdata/reactor/springbootreactor/SpringBootReactorApplication.java
@@ -0,0 +1,45 @@
+package com.nttdata.reactor.springbootreactor;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import reactor.core.publisher.Flux;
+
+@SpringBootApplication
+public class SpringBootReactorApplication implements CommandLineRunner{
+
+	private static final Logger log = LoggerFactory.getLogger(SpringBootReactorApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootReactorApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Flux<String> nombres = Flux.just("Jostin","Mario", "Luis","Guillermo","Oscar")
+			.doOnNext(e -> {
+				if(e.isEmpty()){
+					throw new RuntimeException("Nombres no pueden ser vacios!");
+				}else{
+					System.out.println(e);
+				}
+			});
+
+			nombres.subscribe(e -> log.info(e),
+				error -> log.error(error.getMessage()),
+				new Runnable() {
+
+					@Override
+					public void run() {
+						log.info("Finalizado la ejecucion del Observable");
+						
+					}
+					
+				});
+
+	}
+
+}
